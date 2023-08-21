74e3e6b95fb008b28486106c952f4d1a075bf0b6
pilot0311 <pilot0938@gmail.com>
2023-08-21 22:41:52 +0900

    tennis

--- JavaPro/src/javaProject01/TennisCounter.java
+++ JavaPro/src/javaProject01/TennisCounter.java
@@ -7,7 +7,7 @@ public class TennisCounter {
 	Random rnd = new Random();
 	int numberOfSet;
 	Team team1, team2; 
-	static String[] pointFormat = { "0", "15", "30", "40", "Ad", "Game" };
+	static String[] pointFormat = { "0", "15", "30", "40", "AD", "Game" };
 
 	public TennisCounter(int numberOfSet, Team p1, Team p2) {
 		super();
@@ -17,87 +17,116 @@ public class TennisCounter {
 	}
 
 	public void pointWinner(int p) {
+
+//		if (p == 1) {
+//			team1.point = Math.min(team1.point + 1, 5); // 최대값을 5로 제한
+//			team1.pointStr = pointFormat[team1.point];
+//		} else if (p == 2) {
+//			team2.point = Math.min(team2.point + 1, 5); // 최대값을 5로 제한
+//			team2.pointStr = pointFormat[team2.point];
+//		}
+
 		if (p == 1) {
-			
+
 			team1.point += 1;
+			if (team1.point>4){
+				team1.pointStr = "AD" + (team1.point-3);
+			}else
 			team1.pointStr = pointFormat[team1.point];
+
 		} else if (p == 2) {
-			
+
 			team2.point += 1;
+			if (team2.point>4){
+				team2.pointStr = "AD" + (team2.point-3);
+			}else
 			team2.pointStr = pointFormat[team2.point];
+
 		}
 		// 특정 팀의 포인트 1 증가.
 		// 팀을 선택하는 매개변수 p는 1 또는 2의 값을 가진다.
 	}
-	
-	
-	public void score(Team team1, Team team2, int numberOfSets) {
-		int p = 0;
-		while ( team1.numberOfWonSets != (numberOfSets/2)+1 && team2.numberOfWonSets != (numberOfSets/2)+1) {
-			p = rnd.nextInt(2)+1;
-			pointWinner(p);
-			
-			if (team1.point>=4 || team2.point >=4) {
-				if (team1.point>=3 && team2.point >= 3) {
-					if (Math.abs(team1.point-team2.point) == 2) {
-						if (team1.point>team2.point) {
-							team1.numberOfWongames++;
-							team1.point=0;
-							team2.point=0;
-							team2.pointStr = "0";
-							team1.pointStr = "0";
-						} else {
-							team2.numberOfWongames++;
-							team1.point=0;
-							team2.point=0;
-							team2.pointStr = "0";
-							team1.pointStr = "0";
-						}
-					} else {
-						continue;
-					}
-				} else {
-					if (team1.point>team2.point) {
-						team1.numberOfWongames++;
-						team1.point=0;
-						team2.point=0;
-						team2.pointStr = "0";
-						team1.pointStr = "0";
-					} else {
-						team2.numberOfWongames++;
-						team1.point=0;
-						team2.point=0;
-						team2.pointStr = "0";
-						team1.pointStr = "0";
-					}
-				}
-			} else {
-				continue;
-			} //else
-			
-			if (team1.numberOfWongames == 6) {
-			team1.numberOfWonSets++;
-			team1.point=0;
-			team2.point=0;
-			team1.numberOfWongames = 0;
-			team2.numberOfWongames = 0;
-			team2.pointStr = "0";
-			team1.pointStr = "0";
-		} else if(team2.numberOfWongames == 6){
-			team2.numberOfWonSets++;	
-			team1.point=0;
-			team2.point=0;
-			team1.numberOfWongames = 0;
-			team2.numberOfWongames = 0;
-			team2.pointStr = "0";
-			team1.pointStr = "0";
-		}
-			
-			  
-			dispScoreBoard();
-		}
-	}
-	
+
+
+//	public void pointWinner(int p) {
+//
+////		if (p == 1) {
+////			team1.point = Math.min(team1.point + 1, 5); // 최대값을 5로 제한
+////			team1.pointStr = pointFormat[team1.point];
+////		} else if (p == 2) {
+////			team2.point = Math.min(team2.point + 1, 5); // 최대값을 5로 제한
+////			team2.pointStr = pointFormat[team2.point];
+////		}
+//
+//		if (p == 1) {
+//
+//			team1.point += 1;
+//			//team1.pointStr = pointFormat[team1.point];
+//			team1.pointStr = pointFormat[1];
+//		} else if (p == 2) {
+//
+//			team2.point += 1;
+//			//team2.pointStr = pointFormat[team2.point];
+//			team2.pointStr = pointFormat[1];
+//		}
+//		// 특정 팀의 포인트 1 증가.
+//		// 팀을 선택하는 매개변수 p는 1 또는 2의 값을 가진다.
+//	}
+
+	 public void score(Team team1, Team team2, int numberOfSets) {
+     int p = 0;
+		 int t1 = 0;
+		 int t2 = 0;
+     while (team1.numberOfWonSets != (numberOfSets/2)+1 && team2.numberOfWonSets !=(numberOfSets/2)+1 ) {
+         p = rnd.nextInt(2)+1;
+         pointWinner(p);
+
+         if (team1.point >= 4 && team1.point - team2.point >= 2) {
+             team1.numberOfWongames++;
+             team1.point=0;
+             team2.point=0;
+             team2.pointStr = "0";
+             team1.pointStr = "0";
+			 System.out.println(team1.name + "가 승리 했습니다");
+         } else if (team2.point >= 4 && team2.point - team1.point >= 2){
+             team2.numberOfWongames++;
+             team1.point=0;
+             team2.point=0;
+             team2.pointStr = "0";
+             team1.pointStr = "0";
+			 System.out.println(team2.name + "가 승리 했습니다");
+         }
+
+         if (team1.numberOfWongames >= 6 && team1.numberOfWongames - team2.numberOfWongames >=2) {
+             team1.numberOfWonSets++;
+			  t1 = team1.numberOfWongames;
+			  t2 = team2.numberOfWongames;
+             team1.point=0;
+             team2.point=0;
+             team1.numberOfWongames = 0;
+             team2.numberOfWongames = 0;
+             team2.pointStr = "0";
+             team1.pointStr = "0";
+         } else if(team2.numberOfWongames >= 6 && team2.numberOfWongames - team1.numberOfWongames >=2){
+             team2.numberOfWonSets++;
+			  t1 = team1.numberOfWongames;
+			  t2 = team2.numberOfWongames;
+             team1.point=0;
+             team2.point=0;
+             team1.numberOfWongames = 0;
+             team2.numberOfWongames = 0;
+             team2.pointStr = "0";
+             team1.pointStr = "0";
+         }
+
+         dispScoreBoard();
+     }
+	 System.out.printf("%d, %d", t1, t2);
+
+ }
+
+
+
 //	public void score(Team team1, Team team2, int numberOfSets) {
 //		int p = 0;
 //		while ( team1.numberOfWonSets != (numberOfSets/2)+1 && team2.numberOfWonSets != (numberOfSets/2)+1) {
--- JavaPro/src/javaProject01/TennisGame.java
+++ JavaPro/src/javaProject01/TennisGame.java
@@ -12,20 +12,51 @@ public class TennisGame {
 		int numberOfSets = Integer.parseInt(sc.nextLine());
 		System.out.println("> 단식(1) / 복식(2) 입력 : ");
 		int numberOfTeamMember = Integer.parseInt(sc.nextLine());
-	
-		System.out.println("1번 선수 이름: ");
-		String name = sc.nextLine();
-		Team p1 = new Team(name, 0, 0, 0);
-		System.out.println(p1);
-		
-		System.out.println("2번 선수 이름: ");
-		name = sc.nextLine();
-		Team p2 = new Team(name, p1.point, p1.numberOfWongames, p1.numberOfWonSets);
-		System.out.println(p2);
+		String name;
+		Team p1, p2;
+		TennisCounter tennis = null;
+		switch (numberOfTeamMember){
+			case(1):
+				System.out.println("1번 선수 이름: ");
+				name = sc.nextLine();
+				 p1 = new Team(name, 0, 0, 0);
+				System.out.println(p1);
 
-		TennisCounter tennis = new TennisCounter(numberOfSets, p1, p2);
-		tennis.score(p1, p2, numberOfSets);
-		tennis.dispScoreBoard();
+				System.out.println("2번 선수 이름: ");
+				name = sc.nextLine();
+				 p2 = new Team(name, p1.point, p1.numberOfWongames, p1.numberOfWonSets);
+				System.out.println(p2);
+				 tennis = new TennisCounter(numberOfSets, p1, p2);
+				tennis.score(p1, p2, numberOfSets);
+				tennis.dispScoreBoard();
+			break;
+
+			case (2):
+				System.out.println("1팀 1번 선수 이름: ");
+				name = sc.nextLine();
+				System.out.println("1팀 2번 선수 이름: ");
+				name = name + "/" + sc.nextLine();
+				 p1 = new Team(name, 0, 0, 0);
+				System.out.println(p1);
+
+				System.out.println("2팀 1번 선수 이름: ");
+				name = sc.nextLine();
+
+				System.out.println("2팀 2번 선수 이름: ");
+				name = name + "/" + sc.nextLine();
+				 p2 = new Team(name, p1.point, p1.numberOfWongames, p1.numberOfWonSets);
+				System.out.println(p2);
+				 tennis = new TennisCounter(numberOfSets, p1, p2);
+				tennis.score(p1, p2, numberOfSets);
+				tennis.dispScoreBoard();
+			break;
+		}
+
+	
+
+
+
+
 		
 
 	}
